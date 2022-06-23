01b7ecaf1ac700418b42338a5351371d614abc6d
cooker0910 <cooker98910@gmail.com>
2022-06-23 13:04:11 -0700

    updaet

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 import Web3 from 'web3'
-import { OpenSeaSDK, Network } from 'opensea-js'
+import { OpenSeaPort, OpenSeaSDK, Network } from 'opensea-js'
 import 'dotenv/config';
 
-const main = async() => {
-  console.log('-------------------==========', process.env.YOUR_API_KEY)
-  // This example provider won't let you make transactions, only read-only calls:
-  const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io');
+console.log('-------------------==========', Network.Main)
+// This example provider won't let you make transactions, only read-only calls:
+  const provider = new Web3.providers.HttpProvider('https://eth-mainnet.alchemyapi.io/v2/RPgq1TBdz88OlC6ftNJsqmg6AaHS-qdY');
   
+  const seaport = new OpenSeaPort(provider, {
+    networkName: Network.Main,
+    apiKey: process.env.YOUR_API_KEY
+  })
+
   const openseaSDK = new OpenSeaSDK(provider, {
     networkName: Network.Main,
     apiKey: process.env.YOUR_API_KEY
   })
   
+const main = async() => {
+  console.log(provider);
   const { orders, count } = await openseaSDK.api.getOrders({
-    asset_contract_address: 0x1a92f7381b9f03921564a437210bb9396471050c,
-    token_id: 7578,
-    side: "bid"
+    asset_contract_address: "0x1a92f7381b9f03921564a437210bb9396471050c",
+    token_id: "7578",
+    side: 1
   })
   
   console.log(orders);
 }
-
 
 main()
