b6fc9e23a9355d1162be65013acdb87a23fb2f4b
cooker0910 <cooker98910@gmail.com>
2022-06-24 03:32:10 -0700

    update

--- index.js
+++ index.js
@@ -12,14 +12,24 @@ console.log('-------------------==========', Network.Main)
   })
   
 const main = async() => {
-  console.log("here");
-  const { orders, count } = await seaport.api.getOrders({
-    asset_contract_address: "0x495f947276749ce646f68ac8c248420045cb7b5e",
-    token_id: "15774",
-    side: 0
-  })
+  // console.log("here");
+  // const { orders, count } = await seaport.api.getOrders({
+  //   asset_contract_address: "0x02fa88718f365313e65b406cb39a8fe9e82d1bee",
+  //   token_id: "2",
+  //   side: 0
+  // })
   
-  console.log(orders);
+  // console.log(orders);
+  const sdk = require('api')('@opensea/v1.0#54d5pd7l38yyb63');
+
+  sdk['asset-offers']({
+    limit: '20',
+    asset_contract_address: '0x02fa88718f365313e65b406cb39a8fe9e82d1bee',
+    token_id: '2',
+    'X-API-KEY': process.env.YOUR_API_KEY
+  })
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
 }
 
 main()
