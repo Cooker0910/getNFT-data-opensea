10d3f32dc762777b796c67dc1f351589c804c601
cooker0910 <cooker98910@gmail.com>
2022-07-04 08:42:33 -0700

    update

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 import Web3 from 'web3'
-import { OpenSeaPort, Network } from 'opensea-js'
+import { OpenSeaSDK, Network } from 'opensea-js';
 import 'dotenv/config';
 
-console.log('-------------------==========', Network.Main)
-// This example provider won't let you make transactions, only read-only calls:
-  const provider = new Web3.providers.HttpProvider('https://eth-mainnet.alchemyapi.io/v2/RPgq1TBdz88OlC6ftNJsqmg6AaHS-qdY');
-  
-  const seaport = new OpenSeaPort(provider, {
-    networkName: Network.Main,
-    apiKey: process.env.YOUR_API_KEY
-  })
-  
 const main = async() => {
-  // console.log("here");
-  // const { orders, count } = await seaport.api.getOrders({
-  //   asset_contract_address: "0x02fa88718f365313e65b406cb39a8fe9e82d1bee",
-  //   token_id: "2",
-  //   side: 0
-  // })
-  
-  // console.log(orders);
-  const sdk = require('api')('@opensea/v1.0#54d5pd7l38yyb63');
+const provider = new Web3.providers.HttpProvider('https://eth-mainnet.alchemyapi.io/v2/RPgq1TBdz88OlC6ftNJsqmg6AaHS-qdY');
 
-  sdk['asset-offers']({
-    limit: '20',
-    asset_contract_address: '0x02fa88718f365313e65b406cb39a8fe9e82d1bee',
-    token_id: '2',
-    'X-API-KEY': process.env.YOUR_API_KEY
-  })
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+const seaport = new OpenSeaSDK(provider, {
+  networkName: Network.Main,
+  apiKey: process.env.API_KEY
+})
+
+var params = {
+  protocol: 'seaport',
+  assetContractAddress: "0x1a92f7381b9f03921564a437210bb9396471050c",
+  tokenIds: "7578",
+  side: "bid",
+  limit: 1
+}
+  
+  console.log(seaport, "seaport");
+  // const orders = await seaport.api.getOrders(params)
+  // console.log(orders);
 }
 
 main()
