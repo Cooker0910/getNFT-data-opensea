850ea3a776f5d0e432b871b5e08a72bcbf7fcb7a
cooker0910 <cooker98910@gmail.com>
2022-07-04 20:38:44 -0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-import Web3 from 'web3'
-import { OpenSeaSDK, Network } from 'opensea-js';
-import 'dotenv/config';
+const Web3 = require('web3')
+const { OpenSeaPort, Network, OpenSeaSDK } = require('./Opensea/lib/index');
+require('dotenv').config()
 
-const main = async() => {
-const provider = new Web3.providers.HttpProvider('https://eth-mainnet.alchemyapi.io/v2/RPgq1TBdz88OlC6ftNJsqmg6AaHS-qdY');
+const main = async () => {
+  const provider = new Web3.providers.HttpProvider('https://eth-mainnet.alchemyapi.io/v2/RPgq1TBdz88OlC6ftNJsqmg6AaHS-qdY');
 
-const seaport = new OpenSeaSDK(provider, {
-  networkName: Network.Main,
-  apiKey: process.env.API_KEY
-})
+  const seaport = new OpenSeaSDK(provider, {
+    networkName: Network.Main,
+    apiKey: process.env.API_KEY
+  })
 
-var params = {
-  protocol: 'seaport',
-  assetContractAddress: "0x1a92f7381b9f03921564a437210bb9396471050c",
-  tokenIds: "7578",
-  side: "bid",
-  limit: 1
-}
-  
-  console.log(seaport, "seaport");
-  // const orders = await seaport.api.getOrders(params)
-  // console.log(orders);
+  const { orders, count } = await seaport.api.getOrders({
+    assetContractAddress: "0xed5af388653567af2f388e6224dc7c4b3241c544",
+    tokenId: "1365",
+    side: "bid",
+    limit: 5
+  })
+  console.log(orders, 'here');
 }
 
 main()
